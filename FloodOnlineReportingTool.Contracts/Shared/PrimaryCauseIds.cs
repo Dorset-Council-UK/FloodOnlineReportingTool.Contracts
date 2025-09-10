b146595254e977f358371860c88fd6b6ac5a8ee7
Paul Wittle <paul.wittle@dorsetcouncil.gov.uk>
2025-09-10 10:36:40 +0100

    Split the flood source ids into two classes. Primary and Secondary causes. Version 1.3.2

---
+++ FloodOnlineReportingTool.Contracts/Shared/PrimaryCauseIds.cs
@@ -0,0 +1,19 @@
+﻿namespace FloodOnlineReportingTool.Contracts.Shared;
+
+public static class PrimaryCauseIds
+{
+    // Primary cause Id's
+    public readonly static Guid River = new("018fe08b-a800-7cb2-aa1b-f39aefc48152");
+    public readonly static Guid StreamOrWatercourse = new("018fe08c-9260-77ad-9e3a-418bb047ff5d");
+    public readonly static Guid LakeOrReservoir = new("018fe08d-7cc0-70ce-89e3-78360019c9b7");
+    public readonly static Guid TheSea = new("018fe08e-6720-70ea-9a72-bf9aa0de312b");
+    public readonly static Guid DitchesAndDrainageChannels = new("018fe08f-5180-744d-ab20-b58bf6d04fbb");
+    public readonly static Guid WaterRisingOutOfTheGround = new("018fe090-3be0-7d28-b73d-ba671aa61208");
+    public readonly static Guid FoulDrainageSewerage = new("018fe091-2640-7a8c-ab1e-5c762268a1d7");
+    public readonly static Guid SurfaceWaterDrainage = new("018fe092-10a0-7858-8987-1f84395524b7");
+    public readonly static Guid BlockedRoadDrainage = new("018fe092-fb00-7a34-a8c9-bdac73a1acaa");
+    public readonly static Guid BridgeOrCulvert = new("018fe093-e560-74dc-ac18-f4949190dce3");
+    public readonly static Guid WavesCausedByVehicles = new("018fe094-cfc0-7156-99f5-2fc20e9e19ea");
+    public readonly static Guid RainwaterFlowingOverTheGround = new("018fe095-ba20-7234-b73b-ff8e340dd9fc");
+    public readonly static Guid NotSure = new("018fe096-a480-70d8-91f4-03504bcf926c");
+}
