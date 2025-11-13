6fdeb796438fb87d053d8e46f88ea42b3dabeea9
weolo <matt.mailuk@gmail.com>
2025-11-13 16:31:56 +0000

    New Flood Report search result

---
+++ FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodReport.cs
@@ -0,0 +1,11 @@
+﻿namespace FloodOnlineReportingTool.Contracts.Shared.Search;
+
+public record SearchResultFloodReport(
+    Guid Id,
+    DateTimeOffset CreatedUtc,
+    DateTimeOffset? UpdatedUtc,
+    double Easting,
+    double Northing,
+    DateTimeOffset? ImpactStartUtc,
+    IReadOnlyCollection<SearchResultFloodReportSource> Sources
+);
