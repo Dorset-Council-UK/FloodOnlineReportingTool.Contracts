de9fabd79c3b97afbadcd8bf52d3097fe014ef80
weolo <matt.mailuk@gmail.com>
2025-11-13 16:30:54 +0000

    New Search PagedResults

---
+++ FloodOnlineReportingTool.Contracts/Shared/Search/PagedResult.cs
@@ -0,0 +1,9 @@
+﻿namespace FloodOnlineReportingTool.Contracts.Shared.Search;
+
+public record PagedResult<T>(
+    IReadOnlyCollection<T> Results,
+    int TotalCount,
+    int PageSize,
+    int CurrentPage,
+    int TotalPages
+);
