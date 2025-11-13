41c9d8e2d56dd08de9bdecac52ee70ed545959b6
weolo <matt.mailuk@gmail.com>
2025-11-13 16:45:14 +0000

    Added descriptions

--- FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodReport.cs
+++ FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodReport.cs
@@ -1,5 +1,9 @@
 ﻿namespace FloodOnlineReportingTool.Contracts.Shared.Search;
 
+/// <summary>
+/// Represents a search result for a flood report, including its unique identifier, creation and update timestamps,
+/// data sources.
+/// </summary>
 public record SearchResultFloodReport(
     Guid Id,
     DateTimeOffset CreatedUtc,
--- FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodReportSource.cs
+++ FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodReportSource.cs
@@ -1,3 +1,8 @@
 ﻿namespace FloodOnlineReportingTool.Contracts.Shared.Search;
 
+/// <summary>
+/// Represents the source of a flood report in a search result.
+/// </summary>
+/// <param name="SourceId">The unique identifier of the flood report source.</param>
+/// <param name="Name">A helpful name or description of the flood report source.</param>
 public record SearchResultFloodReportSource(Guid SourceId, string Name);
