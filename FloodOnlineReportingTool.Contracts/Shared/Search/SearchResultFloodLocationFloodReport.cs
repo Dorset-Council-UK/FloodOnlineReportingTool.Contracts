aae1e84745c5b8a21106031691d46cb3e8a7d736
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2025-12-05 18:00:54 +0000

    Search results for flood location flood reports

---
+++ FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodLocationFloodReport.cs
@@ -0,0 +1,12 @@
+﻿namespace FloodOnlineReportingTool.Contracts.Shared.Search;
+
+/// <summary>
+/// Represents a search result for a flood report related to a flood location
+/// </summary>
+public record SearchResultFloodLocationFloodReport(
+    Guid FloodReportId,
+    DateTimeOffset CreatedUtc,
+    DateTimeOffset? UpdatedUtc,
+    DateTimeOffset? ConfirmedUtc,
+    string? ConfirmedUserName
+);
