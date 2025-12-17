07c71a732b8c560206765ea5d5e272a223f488dd
Lucy Bishop <lucy.bishop@dorsetcouncil.gov.uk>
2025-12-17 14:48:21 +0000

    Created two new records

---
+++ FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodProperty.cs
@@ -0,0 +1,16 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace FloodOnlineReportingTool.Contracts.Shared.Search;
+
+/// <summary>
+/// Represents a search result for a flood property
+/// </summary>
+public record SearchResultFloodProperty(
+    Guid Id,
+    DateTimeOffset CreatedUtc,
+    DateTimeOffset? UpdatedUtc,
+    string Title,
+    string Uprn,
+    Point Geom,
+    IReadOnlyCollection<SearchResultFloodPropertyFloodReport> FloodReports
+);
---
+++ FloodOnlineReportingTool.Contracts/Shared/Search/SearchResultFloodPropertyFloodReport.cs
@@ -0,0 +1,13 @@
+﻿namespace FloodOnlineReportingTool.Contracts.Shared.Search;
+
+/// <summary>
+/// Represents a search result for a flood report related to a flood property
+/// </summary>
+public record SearchResultFloodPropertyFloodReport(
+    
+    Guid FloodReportId,
+    DateTimeOffset CreatedUtc,
+    DateTimeOffset? UpdatedUtc,
+    DateTimeOffset? ConfirmedUtc,
+    string? ConfirmedUserName
+);
