709e4ffe114f7510937a122e4ef74d5167f53d63
Paul Wittle <paul.wittle@dorsetcouncil.gov.uk>
2026-02-25 10:26:41 +0000

    Add comment types to shared project

---
+++ FloodOnlineReportingTool.Contracts/Shared/CommentType.cs
@@ -0,0 +1,18 @@
+﻿namespace FloodOnlineReportingTool.Contracts.Shared;
+
+public enum CommentType
+{
+    /// <summary>
+    /// All the comment types available for the FORT system.
+    /// </summary>
+    PublicComment = 0,
+    UserComment,
+    ProblemComment,
+    ImpactComment,
+    DescriptionComment,
+    HistoryComment,
+    CauseComment,
+    SourceDetails,
+    AssistanceComment,
+    AdminComment
+}
