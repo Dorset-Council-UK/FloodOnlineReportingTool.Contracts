ad6c1ab77543d5de66097e79de0fe0a79e90ac65
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-03-11 16:09:28 +0000

    Fixed mistake in topic names

--- FloodOnlineReportingTool.Contracts/Topics/TopicNames.cs
+++ FloodOnlineReportingTool.Contracts/Topics/TopicNames.cs
@@ -7,9 +7,8 @@ public static class TopicNames
     public const string EligibilityCheckCreated = "flood-eligibility-check-created";
     public const string EligibilityCheckUpdated = "flood-eligibility-check-updated";
     public const string FloodReportCreated = "flood-report-created";
-    public const string FloodReportUpdated = "flood-report-updated";
     public const string FloodReportDeleted = "flood-report-deleted";
-    public const string FloodReportStatusUpdated = "flood-report-status-updated";
+    public const string FloodReportUpdated = "flood-report-updated";
     public const string InvestigationCreated = "flood-investigation-created";
     public const string PasswordResetCodeSent = "flood-password-reset-code-sent";
     public const string PasswordResetLinkSent = "flood-password-reset-link-sent";
