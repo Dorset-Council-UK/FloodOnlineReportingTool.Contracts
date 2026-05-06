54ffa38ed7c3f26d729adabb7b774d1ac9992e18
Paul Wittle <paul.wittle@dorsetcouncil.gov.uk>
2026-05-06 14:09:27 +0100

    Make topic names consistent.

--- FloodOnlineReportingTool.Contracts/Topics/TopicNames.cs
+++ FloodOnlineReportingTool.Contracts/Topics/TopicNames.cs
@@ -12,6 +12,6 @@ public static class TopicNames
     public const string FloodReportCreated = "floodreport.management-createreport";
     public const string FloodReportUpdated = "floodreport.management-updatereport";
     public const string FloodReportDeleted = "floodreport.management-deletereport";
-    public const string InvestigationTriggered = "floodreport.management-trigger.investigation";
-    public const string PasswordResetTriggered = "floodreport.management-trigger.passwordreset";
+    public const string InvestigationTriggered = "floodreport.management-investigation";
+    public const string PasswordResetTriggered = "floodreport.management-passwordreset";
 }
