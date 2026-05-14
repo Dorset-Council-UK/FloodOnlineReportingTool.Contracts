454a8326a75671be8ca76dc9fc39fa90a588df2a
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-05-14 16:37:23 +0100

    new topic names using dashes

--- FloodOnlineReportingTool.Contracts/Topics/TopicNames.cs
+++ FloodOnlineReportingTool.Contracts/Topics/TopicNames.cs
@@ -3,14 +3,14 @@ namespace FloodOnlineReportingTool.Contracts.Topics;
 
 public static class TopicNames
 {
-    public const string ActionRequest = "floodreport.status-actionrequest";
-    public const string FloodReportSourceCreated = "floodreport.status-createreport";
-    public const string FloodReportSourceUpdated = "floodreport.status-updatereport";
-    public const string FloodReportSourceDeleted = "floodreport.status-deletereport";
-    public const string InvestigationCreated = "floodreport.status-createinvestigation";
-    public const string VerifyContactTriggered = "floodreport.status-verifycontacttriggered";
-    public const string FloodReportCreated = "floodreport.management-createreport";
-    public const string FloodReportUpdated = "floodreport.management-updatereport";
-    public const string FloodReportDeleted = "floodreport.management-deletereport";
-    public const string InvestigationTriggered = "floodreport.management-investigation";
+    public const string ActionRequest = "floodreport-action-request";
+    public const string FloodReportCreated = "floodreport-created";
+    public const string FloodReportUpdated = "floodreport-updated";
+    public const string FloodReportDeleted = "floodreport-deleted";
+    public const string FloodReportSourceCreated = "floodreport-source-created";
+    public const string FloodReportSourceUpdated = "floodreport-source-updated";
+    public const string FloodReportSourceDeleted = "floodreport-source-deleted";
+    public const string InvestigationCreated = "floodreport-investigation-created";
+    public const string InvestigationTriggered = "floodreport-investigation-triggered";
+    public const string VerifyContactTriggered = "floodreport-verify-contact-triggered";
 }
